f559dd02514aff3deba66facd2667f7d94c6a324
tfuquirk <timothyfquirk@gmail.com>
2019-01-16 10:51:59 -0500

    Done.

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,13 @@
+class PatientsController < ApplicationController
+
+  def index
+    @patients = Patient.all 
+  end
+
+  def show
+    @patient = Patient.find(params[:id])
+    @appointments = @patient.appointments
+  end
+
+
+end
